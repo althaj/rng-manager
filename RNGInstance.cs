994c5bcb4186e08daa4ac32d69f284b76d99a48a
Dávid Florek <33327502+althaj@users.noreply.github.com>
2022-05-15 20:41:55 +0200

    Initial commit
    Contains basic operations, such as creating a new RNG Instance, managing instances

---
+++ RNGInstance.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RNGManager
+{
+    public class RNGInstance
+    {
+        private Random random;
+
+        public string Title { get; private set; }
+        public int Seed { get; private set; }
+
+        public RNGInstance(int? seed = null, string title = "")
+        {
+            Seed = seed.HasValue ? seed.Value : Guid.NewGuid().GetHashCode();
+            Title = !string.IsNullOrEmpty(title) ? title : Seed.ToString();
+            random = new Random(Seed);
+        }
+
+        /// <summary>
+        /// Returns a random bool. You can specify percentage chance.
+        /// </summary>
+        /// <param name="percentage">Chance of true result.</param>
+        /// <returns></returns>
+        public bool NextBool(double percentage = 0.5)
+        {
+            if (percentage < 0 || percentage > 1)
+                throw new ArgumentOutOfRangeException(nameof(percentage), "Percentahe has to be between 0 and 1.");
+
+            return percentage > random.NextDouble();
+        }
+
+        /// <summary>
+        /// Returns a random float. You can specify minimum and maximum values. Minimum is inclusive, maximum is not.
+        /// </summary>
+        /// <param name="min">The returned value will be greater or equal than this minimum.</param>
+        /// <param name="max">The returned value will be lower than this maximum.</param>
+        /// <returns></returns>
+        public float NextFloat(float min = 0, float max = 1f)
+        {
+            return Convert.ToSingle(random.NextDouble()) * (max - min) + min;
+        }
+    }
+}
---
+++ RNGManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RNGManager
+{
+    public class RNGManager
+    {
+        private RNGManager()
+        {
+            rngInstances = new Dictionary<string, RNGInstance>();
+        }
+
+        private static RNGManager main;
+        public static RNGManager Main
+        {
+            get
+            {
+                if (main == null)
+                    main = new RNGManager();
+                return main;
+            }
+        }
+
+        private Dictionary<string, RNGInstance> rngInstances;
+        /// <summary>
+        /// Get an RNG instance by it's title. If there is no instance with this title, create one.
+        /// </summary>
+        /// <param name="title">Title of the instance.</param>
+        /// <returns></returns>
+        public RNGInstance this[string title]
+        {
+            get
+            {
+                if (!rngInstances.ContainsKey(title))
+                    rngInstances.Add(title, new RNGInstance(title: title));
+
+                return rngInstances[title];
+            }
+        }
+
+        /// <summary>
+        /// Get an RNG instance by it's seed. If there is no instance with this seed, create one.
+        /// </summary>
+        /// <param name="seed">The seed of the instance.</param>
+        /// <returns></returns>
+        public RNGInstance this[int seed]
+        {
+            get
+            {
+                var rngInstance = rngInstances.Values.Where(x => x.Seed == seed).FirstOrDefault();
+
+                if (rngInstance == null)
+                    rngInstance = AddInstance(new RNGInstance(seed));
+
+                return rngInstance;
+            }
+        }
+
+        /// <summary>
+        /// Add a new instance to the RNG manager.
+        /// </summary>
+        /// <param name="instance">The RNG instance to be added.</param>
+        public RNGInstance AddInstance(RNGInstance instance)
+        {
+            rngInstances.Add(instance.Title, instance);
+            return this[instance.Title];
+        }
+    }
+}
